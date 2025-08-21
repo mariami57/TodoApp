772f42ea5fd4f9117bb7e2414b85f5c11133b3be
mariami57 <mmitova3@gmail.com>
2025-08-21 18:49:24 +0300

    added tasks model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+
+    STATUS_CHOICES = [
+        ("completed", "Completed"),
+        ("pending", "Pending"),
+    ]
+
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    accomplished_at = models.DateTimeField()
+    status = models.CharField(choices = STATUS_CHOICES, default="pending")
