d038f49b0443845b1ba08a99ee3966341fbccb98
mariami57 <mmitova3@gmail.com>
2025-08-23 17:59:37 +0300

    updated task views logic

--- tasks/views.py
+++ tasks/views.py
@@ -1,34 +1,42 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
+from common.mixins import UserIsCreatorMixin
 from tasks.forms import TaskCreateForm, TaskUpdateForm
 from tasks.models import Task
 
 
 # Create your views here.
-class TaskListView(ListView):
+class TaskListView(ListView, LoginRequiredMixin):
     model = Task
     template_name = "tasks/tasks-list.html"
 
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return Task.objects.all()
+        return Task.objects.filter(user=self.request.user)
 
-class TaskDetailView(DetailView):
+
+class TaskDetailView(DetailView,  LoginRequiredMixin, UserIsCreatorMixin):
     model = Task
     template_name = "tasks/task-details.html"
 
 
-class TaskCreateView(CreateView):
+class TaskCreateView(CreateView,  LoginRequiredMixin, UserIsCreatorMixin):
     model = Task
     form_class = TaskCreateForm
     template_name = "tasks/add-task.html"
     success_url = reverse_lazy("home")
 
 
-class TaskUpdateView(UpdateView):
+class TaskUpdateView(UpdateView,  LoginRequiredMixin, UserIsCreatorMixin):
     model = Task
     form_class = TaskUpdateForm
     template_name = "tasks/edit-task.html"
     success_url = reverse_lazy("home")
 
-class TaskDeleteView(DeleteView):
+class TaskDeleteView(DeleteView,  LoginRequiredMixin, UserIsCreatorMixin):
     model = Task
     success_url = reverse_lazy("home")
