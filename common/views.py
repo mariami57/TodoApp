6d3843e97861bf7eebb31e92405d196957cf979e
mariami57 <mmitova3@gmail.com>
2025-08-24 12:07:32 +0300

    updated home template and view

--- common/views.py
+++ common/views.py
@@ -5,9 +5,19 @@ from tasks.models import Task
 
 # Create your views here.
 class HomeView(ListView):
-    template_name = 'common/home.html'
+    template_name = "common/home.html"
     model = Task
 
     def get_queryset(self):
         user = self.request.user
         return Task.objects.filter(user=user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        user = self.request.user
+
+        context["pending_tasks"] = Task.objects.filter(user=user, status="pending")
+        context["completed_tasks"] = Task.objects.filter(user=user, status="completed")
+
+        return context
