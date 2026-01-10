7a63b18a92486d2c45b56b76a3b2b02675574a41
mariami57 <mmitova3@gmail.com>
2026-01-10 12:35:07 +0200

    updated requirements.txt and database settings

--- TodoApp/settings.py
+++ TodoApp/settings.py
@@ -14,6 +14,9 @@ from pathlib import Path
 
 from django.urls import reverse_lazy
 
+import os
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -102,16 +105,11 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+    )
 }
-
-DATABASES["default"]["OPTIONS"] = {
-    "timeout": 20,
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
